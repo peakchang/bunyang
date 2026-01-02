9af0e0c3c35c9e65a2d1d5259a862ee0a6705cc6
rider <ridebus82@nate.com>
2026-01-02 13:52:25 +0900

    260102 asdflijogogogogo

---
+++ ecosystem.site-b.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps: [
+        {
+            name: "api",
+            cwd: "./apps/api/app",          // 너 실제 폴더명으로 수정
+            script: "server.js",   // API build 산출물 경로로 수정
+            exec_mode: "fork",
+            instances: 1,
+            env: {
+                NODE_ENV: "production",
+                PORT: 4000
+            }
+        },
+        {
+            name: "site-a",
+            cwd: "./apps/site-a",
+            script: "server.js",   // SvelteKit adapter-node 기준
+            exec_mode: "fork",
+            instances: 1,
+            env: {
+                NODE_ENV: "production",
+                PORT: 4010
+            }
+        }
+    ]
+};
