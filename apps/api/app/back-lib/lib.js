de857f336116dc556c599e11c3294be2fb57ceef
rider <ridebus82@nate.com>
2026-01-05 14:19:09 +0900

    260105 asdfkhigioogogogo

--- apps/api/app/back-lib/lib.js
+++ apps/api/app/back-lib/lib.js
@@ -96,7 +96,7 @@ export function mailSender(reciever, subject, content) {
 
 export async function aligoKakaoNotification_formanager(req, customerInfo) {
 
-    console.log('Kakao Message Send Is Started!!!!!!!!!!!!!!!!');
+    // console.log('Kakao Message Send Is Started!!!!!!!!!!!!!!!!');
     // console.log(process.env.ALIGOKEY);
     // console.log(process.env.ALIGOID);
     // console.log(process.env.ALIGO_SENDERKEY);
@@ -132,27 +132,16 @@ export async function aligoKakaoNotification_formanager(req, customerInfo) {
             }
         })
 
-        console.log(result);
-        console.log(customerInfo);
-
         req.body = {
             senderkey: process.env.ALIGO_SENDERKEY,
-            tpl_code: 'UA_7459',
+            tpl_code: 'TX_0641',
             token: result.token,
             sender: '010-6628-6651',
             receiver_1: customerInfo.ciPhone,
             subject_1: '분양정보 신청고객 알림톡',
-            // message_1: `고객 접수 안내!\n${customerInfo.ciSite} ${customerInfo.ciName} 접수되었습니다.\n고객 번호 : ${customerInfo.ciReceiver}`,
-            message_1: `${customerInfo.ciSite}고객 유입 알림!\n\n고객명:${customerInfo.ciName}\n연락처:${customerInfo.ciReceiver}\n\n※ 상담 대기 상태입니다.\n빠르게 컨택 진행 부탁 드립니다.`,
-            button_1: {
-                "button": [{
-                    "name": "채널 추가",
-                    "linkType": "AC"
-                }]
-            }
+            message_1: `고객 접수 안내!\n${customerInfo.ciSite} ${customerInfo.ciName} 접수되었습니다.\n고객 번호 : ${customerInfo.ciReceiver}`,
 
         }
-        console.log(req.body);
 
         let resultSend = await new Promise((resolve, reject) => {
             if (true) {
@@ -183,6 +172,97 @@ export async function aligoKakaoNotification_formanager(req, customerInfo) {
 
     }
 }
+
+
+// export async function aligoKakaoNotification_formanager(req, customerInfo) {
+
+//     console.log('Kakao Message Send Is Started!!!!!!!!!!!!!!!!');
+//     // console.log(process.env.ALIGOKEY);
+//     // console.log(process.env.ALIGOID);
+//     // console.log(process.env.ALIGO_SENDERKEY);
+
+//     try {
+//         const AuthData = {
+//             apikey: process.env.ALIGOKEY,
+//             // 이곳에 발급받으신 api key를 입력하세요
+//             userid: process.env.ALIGOID,
+//             // 이곳에 userid를 입력하세요
+//         }
+
+//         req.body = {
+//             type: 'i',  // 유효시간 타입 코드 // y(년), m(월), d(일), h(시), i(분), s(초)
+//             time: 1, // 유효시간
+//         }
+//         // console.log('req.body', req.body)
+
+//         const result = await new Promise((resolve, reject) => {
+//             if (true) {
+//                 aligoapi.token(req, AuthData)
+//                     .then((r) => {
+//                         // console.log('alligo', r);
+//                         resolve(r);
+//                     })
+//                     .catch((e) => {
+//                         // console.error('err', e)
+//                         reject(e)
+//                     })
+//             } else {
+//                 // console.log(2)
+//                 resolve(true)
+//             }
+//         })
+
+//         console.log(result);
+//         console.log(customerInfo);
+
+//         req.body = {
+//             senderkey: process.env.ALIGO_SENDERKEY,
+//             tpl_code: 'UA_7459',
+//             token: result.token,
+//             sender: '010-6628-6651',
+//             receiver_1: customerInfo.ciPhone,
+//             subject_1: '분양정보 신청고객 알림톡',
+//             // message_1: `고객 접수 안내!\n${customerInfo.ciSite} ${customerInfo.ciName} 접수되었습니다.\n고객 번호 : ${customerInfo.ciReceiver}`,
+//             message_1: `${customerInfo.ciSite}고객 유입 알림!\n\n고객명:${customerInfo.ciName}\n연락처:${customerInfo.ciReceiver}\n\n※ 상담 대기 상태입니다.\n빠르게 컨택 진행 부탁 드립니다.`,
+//             button_1: {
+//                 "button": [{
+//                     "name": "채널 추가",
+//                     "linkType": "AC"
+//                 }]
+//             }
+
+//         }
+//         console.log(req.body);
+
+//         let resultSend = await new Promise((resolve, reject) => {
+//             if (true) {
+
+//                 // console.log('kakao send arrived~~!!');
+//                 // console.log(req.body);
+//                 // console.log(AuthData);
+//                 aligoapi.alimtalkSend(req, AuthData).then((r) => {
+//                     // console.log('alligo', r);
+//                     console.log('kakao send is success!!!!!!!!!!!!');
+//                     resolve(true);
+//                 }).catch((e) => {
+//                     console.error('err', e)
+//                     console.log('kakao send is false T.T');
+//                     reject(false);
+//                 })
+//             } else {
+//                 console.log('kakao send is false T.T');
+//                 // console.log(2)
+//                 resolve(true)
+//             }
+//         })
+//     } catch (e) {
+//         // await db.rollback(connection);
+//         // next(e);
+//         console.error(e.message);
+//         console.log('kakao send is error!!!! (in back lib) T.T');
+
+//     }
+// }
 
 
 export async function aligoKakaoNotification_detail(req, sendMessageObj) {
